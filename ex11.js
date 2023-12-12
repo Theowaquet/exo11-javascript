5502d008608d3a3aced2f6d02e60c222799e887b
Theowaquet <theo.waq@gmail.com>
2023-12-12 17:02:03 +0100

    exo 11

---
+++ ex11.js
@@ -0,0 +1,14 @@
+var MaDiv = document.getElementById("div1");
+MaDiv.addEventListener("click", test);
+
+function test(evenement) {
+    console.log("ok");
+    fetch(
+        "http://192.168.65.64/main.json",
+        {method : 'GET'}
+        ).then(
+            response => response.json()
+        ).then(
+            response2 =>evenement.target.innerHTML = JSON.stringify(response2)
+        ).catch(error => console.log("erreur : "+ error));
+}scroll
